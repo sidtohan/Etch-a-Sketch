f8d14bad063844c4b0680f5c85075ab3518b449a
Siddhant Tohan <siddhant.tohan@gmail.com>
2021-10-24 13:29:58 +0530

    Added the logic for clearing, and made some visual changes to the ui

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const containerSize = 900 * 256;
 function initializeGrid(size) {
   // selecting the grid container
   const gridContainer = document.querySelector('.grid-container');
-  const boxSize = Math.sqrt(containerSize)/size;
+  const boxSize = Math.sqrt(containerSize) / size;
   // setting the required specifications
-  gridContainer.setAttribute('style', 
-  `display: grid; 
+  gridContainer.setAttribute('style',
+    `display: grid; 
   grid-template-columns: repeat(${size},${boxSize}px);
   grid-auto-rows: ${boxSize}px;`)
 
@@ -21,6 +21,7 @@ function initializeGrid(size) {
   }
 }
 
+// function for adding listeners to the grid element
 function addGridListeners() {
   let gridItemList = document.querySelectorAll('.grid-item');
   gridItemList.forEach(item => {
@@ -32,13 +33,23 @@ function setGridColor(e) {
   e.target.style.backgroundColor = "black";
 }
 
-function newGrid(){
+// clears the grid
+function clearGrid() {
+  const coloredBoxes = document.querySelectorAll('.grid-item');
+  coloredBoxes.forEach(box => {
+    if(box.getAttribute('style')){
+      box.removeAttribute('style');
+    }
+  });
+}
+
+function newGrid() {
   let user;
   do {
     user = parseInt(prompt("Enter the number of squares per side: "));
-    if (user >= 100){
+    if (user >= 100) {
       alert("Please enter a size < 100");
-    } 
+    }
   } while (user >= 100);
 
 
@@ -63,5 +74,5 @@ initializeGrid(16);
 addGridListeners();
 
 // setting up the button
-const clearButton = document.querySelector('.clear-grid');
-clearButton.onclick = newGrid;
+const btn = document.querySelector('#clear-grid');
+btn.addEventListener('click', clearGrid);
