2e41282baaab7a04f0059131069c6e632e934de6
Siddhant Tohan <siddhant.tohan@gmail.com>
2021-10-26 21:34:06 +0530

    Made some major changes to the ui, and also cleaned the js code a little bit as well

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+// Global Constants
 const containerSize = 900 * 256;
+const displaySize = document.querySelector('.display-size');
+const slider = document.querySelector('.slider');
+const btn = document.querySelector('#clear-grid');
+const gridContainer = document.querySelector('.grid-container');
 
+
+// Functions
 function initializeGrid(size) {
-  const gridContainer = document.querySelector('.grid-container');
   gridContainer.setAttribute('style',
     `display: grid; 
   height: 480px;
@@ -11,10 +17,9 @@ function initializeGrid(size) {
   overflow: hidden;
   `)
 
-  // using for loop to add specified number of boxes
+  // creating the required size by using nested for loop
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
-      // creating the required div
       let gridItem = document.createElement('div');
       gridItem.classList.add("grid-item");
       gridContainer.appendChild(gridItem);
@@ -30,6 +35,7 @@ function addGridListeners() {
   });
 }
 
+// sets the color of grid item on hovering
 function setGridColor(e) {
   e.target.style.backgroundColor = "black";
 }
@@ -39,41 +45,47 @@ function clearGrid(e) {
   const coloredBoxes = document.querySelectorAll('.grid-item');
   e.target.classList.add('clear-grid-animate');
   coloredBoxes.forEach(box => {
-    if(box.getAttribute('style')){
+    if (box.getAttribute('style')) {
       box.removeAttribute('style');
     }
   });
 }
 
-function animateButton(e){
-  if(e.propertyName != 'transform') return;
+// animates the clear grid button
+function animateButton(e) {
+  if (e.propertyName != 'transform') return;
   e.target.classList.remove('clear-grid-animate');
 }
 
+// creates a new grid by a) clearing it, and b) calling the intialized
+// grid function with the required size
 function newGrid(size) {
   const gridContainer = document.querySelector('.grid-container');
-  // Clearing the grid container
-  gridContainer.innerHTML = "";
 
-  // intializing the new grid
+  // grid cleared by setting inner html as empty string
+  gridContainer.innerHTML = "";
   initializeGrid(size);
+
   addGridListeners();
   // need to add listeners again
 
 }
 
-// intializing the grid
+// detects when the mouse has released the slider
+function stopSlider(e) {
+  newGrid(e.target.value);
+}
+
+function updateDisplaySize(e){
+  displaySize.textContent = `${e.target.value}x${e.target.value}`;
+}
+
+
+// intial code and event listeners added
 initializeGrid(16);
 addGridListeners();
 
-// setting up the button
-const btn = document.querySelector('#clear-grid');
 btn.addEventListener('click', clearGrid);
 btn.addEventListener('transitionend', animateButton);
-
-const displaySize = document.querySelector('.display-size');
-const slider = document.querySelector('.slider');
-slider.oninput = function() {
-  newGrid(this.value);
-  displaySize.textContent = `${this.value}`;
-}
+slider.addEventListener('mouseup', stopSlider);
+slider.addEventListener('input', updateDisplaySize);
